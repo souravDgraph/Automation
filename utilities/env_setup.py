8a86979c139e0e41b421c92617d0786b04cd5db0
kaushik691 <74699127+kaushik691@users.noreply.github.com>
2020-12-28 10:41:21 +0530

    Updated error in the script

--- utilities/env_setup.py
+++ utilities/env_setup.py
@@ -31,8 +31,9 @@ def main(argv):
             else:
                 pip_name = 'pip3'
                 python_name = 'python3'
-
+    
             subprocess.check_call([pip_name, 'install', '-r', '../requirements.txt'])
+            proj_name = arg_value
             if proj_name == "Dgraph":
                 proj_lib_paths = ['./lib/dgraph_lib/']
             elif proj_name == "Slash":
