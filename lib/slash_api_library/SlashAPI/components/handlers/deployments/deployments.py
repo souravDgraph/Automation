3208038c6585e738ce20891ca5503bc900c0e828
vivetha-madesh <73867046+vivetha-madesh@users.noreply.github.com>
2021-02-24 11:07:18 +0530

    Update deployments.py

--- lib/slash_api_library/SlashAPI/components/handlers/deployments/deployments.py
+++ lib/slash_api_library/SlashAPI/components/handlers/deployments/deployments.py
@@ -205,7 +205,6 @@ class Deployments():
         connection.create_session(session_alias, url, auth)
         for cycle in range(1, 20):
             response_code = requests.get(url, headers=auth)
-            logger.info("Ib hete")
             logger.info(response_code)
             logger.info(type(response_code.status_code))
             if int(response_code.status_code) == expected_response:
